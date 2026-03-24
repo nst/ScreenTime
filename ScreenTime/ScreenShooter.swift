091a8a8059fd352a258f11129c0fdaaedf6f9f46
Nicolas Seriot <nicolas@seriot.ch>
2026-03-24 14:58:07 +0100

    misc

--- ScreenTime/ScreenShooter.swift
+++ ScreenTime/ScreenShooter.swift
@@ -93,7 +93,7 @@ class ScreenShooter {
 
         Task {
             do {
-                let content = try await SCShareableContent.excludingDesktopWindows(false, onScreenWindowsOnly: true)
+                let content = try await SCShareableContent.current
                 let displays = content.displays
 
                 for display in displays {
